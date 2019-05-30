c4efe9d320a1299362753c4022e4e1a50b545979
nthanhvy <nguyenthithanhvyjps15@gmail.com>
2019-05-30 21:40:41 +0700

    add some changes

--- API_server/controllers/db.js
+++ API_server/controllers/db.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: 'postgres',
-    host: 'localhost',
+    host: '18.188.223.17',
     port: 5432,
     database: 'farcryonline',
     password: null
